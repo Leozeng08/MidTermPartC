dcf27da800523cba0523661c93925b0cf136e3ce
Leozeng08 <zengliang0528@gmail.com>
2022-06-21 12:56:25 -0400

    this is my name branch

---
+++ src/LiangZeng.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+// this is my name class.
+/**
+ *
+ * @author liangzeng
+ */
+public class LiangZeng {
+    public static void main(String[] args) {
+        System.out.println("this is my name class");
+    }
+        
+    
+}
